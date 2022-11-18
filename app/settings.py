2474d27e90799deb23db6961b899e5b0e067e39c
kimhyunsoo <pip4396@naver.com>
2022-11-18 23:43:32 +0900

    update

--- app/settings.py
+++ app/settings.py
@@ -145,9 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
