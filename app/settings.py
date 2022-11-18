cc2ae1c31b5a581a731500db26eadaee8fd56a12
kimhyunsoo <pip4396@naver.com>
2022-11-19 02:37:14 +0900

    update

--- app/settings.py
+++ app/settings.py
@@ -36,9 +36,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['kimhyunsoo.pythonanywhere.com']
+# ALLOWED_HOSTS = ['kimhyunsoo.pythonanywhere.com']
 
 
 # Application definition
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
